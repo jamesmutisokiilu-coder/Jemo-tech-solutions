b23a0ff8f3a918cfc6ca8e6ce5f08366ba45189c
jamesmutisokiilu-coder <jamesmutisokiilu@gmail.com>
2026-06-01 17:28:16 +0300

    Update app.py

--- app.py
+++ app.py
@@ -171,10 +171,7 @@ def services():
 
 @app.route("/admin")
 def admin():
-    if "user" not in session:
-        return redirect(url_for("login"))
-
-    return render_template("admin.html")
+    return render_template("admin_login.html")
 
 @app.route("/portfolio")
 def portfolio():
